8976e022ef0d9f38f8c546849a04b6b05e1358b9
Ssioo <41153640+Ssioo@users.noreply.github.com>
2019-09-16 23:02:12 +0900

    02-Front-end. 16. event delegation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-const heart = document.querySelector('.heart_btn');
 const header = document.querySelector('#header');
 const sidebox = document.querySelector('.side_box');
 const variableWidth = document.querySelectorAll('.contents_box .contents');
+const delegation = document.querySelector('.contents_box');
 
-heart.addEventListener('click', function () {
-    heart.classList.toggle('on');
-})
+
+setTimeout(function () {
+    scrollTo(0, 0);
+}, 100)
 
 window.addEventListener('resize', function () {
     if (matchMedia('screen and (max-width:800px)').matches) {
@@ -14,7 +15,9 @@ window.addEventListener('resize', function () {
         }
     } else {
         for (let i = 0; i < variableWidth.length; i++) {
-            variableWidth[i].removeAttribute('style');
+            if (window.innerWidth > 600) {
+                variableWidth[i].removeAttribute('style');
+            }
         }
     }
 })
@@ -22,15 +25,42 @@ window.addEventListener('resize', function () {
 window.addEventListener('scroll', function () {
     if (pageYOffset >= 10){
         header.classList.add('on');
-        sidebox.classList.add('on');
-
-        let calcWidth = window.innerWidth / 2 + 167;
-        sidebox.style.left = calcWidth + 'px';
-
+        if (sidebox) {
+            sidebox.classList.add('on');
+            let calcWidth = window.innerWidth / 2 + 167;
+            sidebox.style.left = calcWidth + 'px';
+        }
     } else{
         header.classList.remove('on');
-        sidebox.classList.remove('on');
-        sidebox.removeAttribute('style');
+        if (sidebox) {
+            sidebox.classList.remove('on');
+            sidebox.removeAttribute('style');
+        }
 
     }
 });
+
+if (delegation) {
+    delegation.addEventListener('click', function (e) {
+        let elem = e.target;
+
+        while (!elem.getAttribute('data-name')){
+            elem = elem.parentNode;
+            if (elem.nodeName === 'BODY') {
+                elem = null;
+                return;
+            }
+        }
+        if (elem.matches('[data-name="heartbeat"]')){
+
+        } else if (elem.matches('[data-name="bookmark"]')){
+
+        } else if (elem.matches('[data-name="share"]')){
+
+        } else if (elem.matches('[data-name="more"]')){
+
+        }
+
+        elem.classList.toggle('on');
+    })
+}
